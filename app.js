acae713a0071748f6ccad58b876c5417d4654d1a
Ankit Pawar <01ankitpawar@gmail.com>
2023-07-30 14:51:06 +0530

    typo fixed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 //
 app.use(cors({
   origin:[process.env.FRONTEND_URL],
-  method: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }))
 
